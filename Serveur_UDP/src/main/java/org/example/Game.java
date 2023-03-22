f17065a3e8f6787df7115e2b7cc9fbf686d91fe9
BuildTools <unconfigured@null.spigotmc.org>
2023-03-22 21:14:17 +0100

    Added anagramicClasse

--- Serveur_UDP/src/main/java/org/example/Game.java
+++ Serveur_UDP/src/main/java/org/example/Game.java
@@ -5,7 +5,8 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Game {
 
@@ -14,6 +15,7 @@ public class Game {
     private final int finalLength;
     private final ArrayList<String> anagramSequence;
     private ArrayList<String> dictionary;
+    private Map<String, Set<String>> anagramicClasses;
     public Game(InetAddress userAddr, int userPort, int finalLength){
         this.userAddr = userAddr;
         this.userPort = userPort;
@@ -23,7 +25,34 @@ public class Game {
 
         this.anagramSequence = findAnagramSequence(finalLength);
 
+        anagramicClasses = createAnagramicClasses(dictionary);
+
     }
+
+    private Map<String, Set<String>> createAnagramicClasses(ArrayList<String> dictionary){
+
+        Map<String, Set<String>> anagramicClasses = new HashMap<String, Set<String>>();
+
+        for(String word : dictionary){
+            char wordArray[] = word.toCharArray();
+            Arrays.sort(wordArray);
+            String sortedWord = new String(wordArray);
+
+            Set<String> newSet;
+            if(anagramicClasses.containsKey(sortedWord)){
+                newSet = anagramicClasses.get(sortedWord);
+            }else{
+
+                newSet = new HashSet<>();
+            }
+            newSet.add(word);
+            anagramicClasses.put(sortedWord, newSet);
+
+        }
+
+        return anagramicClasses;
+    }
+
 
     public int getUserPort(){
         return this.userPort;
@@ -59,32 +88,46 @@ public class Game {
     private ArrayList<String> findAnagramSequence(int maxLength){
         ArrayList<String> anagramSequence = new ArrayList<>();
 
-        boolean lengthIsOk = false;
-
         if(dictionary.size()==0){
 
             System.err.println("Dictionary is empty -> cannot create anagram sequence");
             return anagramSequence;
         }
 
-        String lastWord = "";
+        ArrayList<String> potentialLastWords = new ArrayList<>();
 
         /* Checking if there is a word of specified max length */
         for(String word : dictionary){
             if(word.length() == maxLength){
-                lastWord = word;
-                lengthIsOk = true;
+                potentialLastWords.add(word);
             }
         }
 
-        if(!lengthIsOk){
+        if(potentialLastWords.isEmpty()){
             System.err.println("Could not find a word with a max length of " + maxLength + " -> cannot create anagram sequence");
             return anagramSequence;
         }
 
-        anagramSequence.add(lastWord);
+        /* Choosing one of the last words randomly */
+        int randomLastWord = ThreadLocalRandom.current().nextInt(0, potentialLastWords.size());
+
+        String lastWord = potentialLastWords.get(randomLastWord);
+
+        ArrayList<String> initSequence = new ArrayList<>();
+        initSequence.add(lastWord);
+
+        int length = 1;
+
 
         return anagramSequence;
+    }
+
+    private void findAnagrams(String initialWord, ArrayList<String> sequence){
+
+        ArrayList<String> anagrams = new ArrayList<>();
+
+        
+
     }
 
 }
